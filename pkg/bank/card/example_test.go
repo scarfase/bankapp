4ee010d8fb87183fb5a0132e0ea2efcf321d7dda
Dmitriy Aitkulov <scarfase1989@gmail.com>
2021-10-17 22:03:24 +0600

    Add test for negative balance, inactive card and many cards in Total func

--- pkg/bank/card/example_test.go
+++ pkg/bank/card/example_test.go
@@ -6,13 +6,38 @@ import (
 )
 
 func ExampleTotal() {
-	cards := []types.Card{
+	fmt.Println(Total([]types.Card{
 		{
 			Balance: 1_000_00,
 			Active:  true,
 		},
-	}
-	fmt.Println(Total(cards))
-	//Output: 100000
+	}))
+	fmt.Println(Total([]types.Card{
+		{
+			Balance: 1_000_00,
+			Active:  true,
+		},
+		{
+			Balance: 2_000_00,
+			Active:  true,
+		},
+	}))
+	fmt.Println(Total([]types.Card{
+		{
+			Balance: 1_000_00,
+			Active:  false,
+		},
+	}))
+	fmt.Println(Total([]types.Card{
+		{
+			Balance: -1_000_00,
+			Active:  true,
+		},
+	}))
 
+	// Output:
+	// 100000
+	// 300000
+	// 0
+	// 0
 }
